b128c05ae1220a23eca334b01da92cd41c03c36a
JosipCunko <ultrabrzitranzijent@gmail.com>
2026-01-31 19:22:10 +0100

    v-langchain

--- .ipynb_checkpoints/config-checkpoint.py
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -1,1 +1,3 @@
-apiKey = "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"
+# openrouter.ai
+# disabled when its not used of course, so its not a problem if its here
+apiKey = "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"
--- config.py
+++ config.py
@@ -1,1 +1,3 @@
+# openrouter.ai
+# disabled when its not used of course, so its not a problem if its here
 apiKey = "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"
