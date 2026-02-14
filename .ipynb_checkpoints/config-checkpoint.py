5f62ad913e9858eb2d19bc475b725ad0fd7b27ac
JosipCunko <ultrabrzitranzijent@gmail.com>
2026-02-14 20:04:17 +0100

    langchain module

--- .ipynb_checkpoints/config-checkpoint.py
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -1,3 +1,4 @@
 # openrouter.ai
 # disabled when its not used of course, so its not a problem if its here
 apiKey = "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"
+apiKey2 = "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" 
